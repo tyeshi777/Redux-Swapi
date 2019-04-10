3375c9c1dd08ee8e264e601894e807b3198bd016
Yeshi <tyeshi777@yahoo.com>
2019-04-10 14:48:08 -0700

    first commit after deleting the repo and recloning

--- src/index.js
+++ src/index.js
@@ -3,16 +3,19 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { Provider } from "react-redux";
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import rootReducer from "./reducers";
-// needed dependancies
+import { thunk } from "redux-thunk";
+import { logger } from "redux-logger";
+// needed dependencies
 // applyMiddleware from redux
 // thunk from redux-thunk
 // logger from redux-logger
 // rootReducer from ./reducers
 
 const store = createStore(
-  rootReducer
+  rootReducer,
+  applyMiddleware(thunk, logger)
   /* applyMiddleware goes here */
 );
 
